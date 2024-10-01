6db82b301bc02e8fd0d4e4cc2ae305f2a1556c49
Jérémy LANOUE <jlanoue.18@gmail.com>
2024-10-01 11:56:04 +0200

    added getter history

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoveService } from '../love.service';
 
 @Component({
   selector: 'app-history',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private service: LoveService
+  ) { }
 
   ngOnInit() {
+  }
+
+  get history() {
+    return this.service.history
   }
 
 }
